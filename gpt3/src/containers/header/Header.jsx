6dc7ee0ea88672d063cc87f4e714778b5fe58827
Steven Croft <scroft001@gmail.com>
2022-10-06 17:25:19 -0700

    Header component started

--- gpt3/src/containers/header/Header.jsx
+++ gpt3/src/containers/header/Header.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './header.css'
+import ai from '../../assets/ai.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header-container'>
+      <div className="header-content">
+        <div className="header-heading">
+          <h1>Let's Build Something amazing with GPT-3 OpenAI</h1>
+        </div>
+        <div className="header-description">
+          <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        </div>
+        <div className="header-form">
+          <form>
+            <input type="text" className="header-form-input" placeholder="Your Email Address"/>
+          </form>
+          <button type='button'>Get Started</button>
+        </div>
+        <div className='header-signup'>
+
+        </div>
+      </div>
+      <div className="header-illustration">
+        <img src={ai} alt="ai" />
+      </div>
+    </div>
   )
 }
 
