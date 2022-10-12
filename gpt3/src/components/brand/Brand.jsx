c30b75823bfe2898f025112df9e2fc17f00a4489
Steven Croft <scroft001@gmail.com>
2022-10-11 21:32:50 -0700

    mapped brand logos

--- gpt3/src/components/brand/Brand.jsx
+++ gpt3/src/components/brand/Brand.jsx
@@ -6,10 +6,12 @@ import dropbox from '../../assets/dropbox.png';
 import slack from '../../assets/slack.png';
 import shopify from '../../assets/shopify.png';
 
+const brands = [google, slack, atlassian, dropbox, shopify];
+
 const Brand = () => {
   return (
-    <div>
-      Brand
+    <div className='brand-row'>
+      {brands.map(brand => <img src={brand} alt={brand} />)}
     </div>
   )
 }
