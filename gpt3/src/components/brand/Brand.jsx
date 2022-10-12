1edfe3913f18cfbe47df6ddea6c0b85758b200a1
Steven Croft <scroft001@gmail.com>
2022-10-11 21:28:12 -0700

    imported assets

--- gpt3/src/components/brand/Brand.jsx
+++ gpt3/src/components/brand/Brand.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import './brand.css';
+import atlassian from '../../assets/atlassian.png';
+import google from '../../assets/google.png';
+import dropbox from '../../assets/dropbox.png';
+import slack from '../../assets/slack.png';
+import shopify from '../../assets/shopify.png';
 
 const Brand = () => {
   return (
