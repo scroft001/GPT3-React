5cb2199d629ba1cc5e2e118b46d4683189c0a646
Steven <scroft001@gmail.com>
2022-10-12 17:40:25 -0700

    Started the what section

--- gpt3/src/containers/whatGPT3/WhatGPT3.jsx
+++ gpt3/src/containers/whatGPT3/WhatGPT3.jsx
@@ -3,7 +3,19 @@ import './whatGPT3.css'
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+    <div className='what-container'>
+      <div className='what-row-top'>
+        <h2 className="what-heading">What is GPT-3</h2>
+        <p>We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.</p>
+      </div>
+      <div className='what-row-middle'>
+        <h2>The possibilities are beyond your imagination</h2>
+        <p>Explore The Library</p>
+      </div>
+      <div className='what-row-bottom'>
+        
+      </div>
+    </div>
   )
 }
 
