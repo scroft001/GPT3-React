28f578aa8b70e4c235d884248a153029577cce52
Steven Croft <scroft001@gmail.com>
2022-10-17 22:04:53 -0700

    Added a to do item

--- gpt3/src/containers/whatGPT3/WhatGPT3.jsx
+++ gpt3/src/containers/whatGPT3/WhatGPT3.jsx
@@ -5,6 +5,7 @@ const WhatGPT3 = () => {
   return (
     <div className='what-container'>
       <div className='what-row-top'>
+        {/* Todo: refactor as a feature component with props */}
         <h2 className="what-heading">What is GPT-3</h2>
         <p>We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.</p>
       </div>
