e00486577d6e9d623647b0e01724c4a4f3c3227b
Steven <scroft001@gmail.com>
2022-10-24 16:48:25 -0700

    Possibility section finished

--- gpt3/src/containers/possibility/Possibility.jsx
+++ gpt3/src/containers/possibility/Possibility.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './possibility.css'
+import possiblity from '../../../src/assets/possibility.png'
 
 const Possibility = () => {
   return (
-    <div>Possibility</div>
+    <div className='possibility-container'>
+      <div className='possibility-column possibility-column-left'>
+        <img src={possiblity} alt={"Girl in VR headset"} />
+      </div>
+      <div className='possibility-column possibility-column-right'>
+        <p id='blue'>Request Early Access to Get Started</p>
+        <h2>The possibilities are beyond your imagination</h2>
+        <p id='main'>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        <p id='orange'>Request Early Access to Get Started</p>
+      </div>
+    </div>
   )
 }
 
