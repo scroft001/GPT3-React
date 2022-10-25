a19ee289e46348dc7ec365d05cc3817ad658863c
Steven <scroft001@gmail.com>
2022-10-24 17:21:28 -0700

    CTA finished

--- gpt3/src/components/cta/Cta.jsx
+++ gpt3/src/components/cta/Cta.jsx
@@ -3,8 +3,14 @@ import './cta.css';
 
 const Cta = () => {
   return (
-    <div>
-      CTA
+    <div className='cta-container'>
+      <div className="cta-text">
+        <p>Request Early Access to Get Started</p>
+        <h2>Register today & start exploring the endless possiblities.</h2>
+      </div>
+      <div className="cta-button">
+        <p>Get Started</p>
+      </div>
     </div>
   )
 }
