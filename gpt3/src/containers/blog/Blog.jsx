890428c89f922bc74c628c9af85d9ce47a50c54b
Steven <scroft001@gmail.com>
2022-10-24 17:34:49 -0700

    Started blog

--- gpt3/src/containers/blog/Blog.jsx
+++ gpt3/src/containers/blog/Blog.jsx
@@ -3,7 +3,9 @@ import './blog.css';
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='blog-container'>
+      <h2>A lot is happening, We are blogging about it.</h2>
+    </div>
   )
 }
 
