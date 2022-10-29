8a7365e8af130f8da9143c303f888866763312cd
Steven Croft <scroft001@gmail.com>
2022-10-28 22:15:21 -0700

    Switched blog to components using props

--- gpt3/src/containers/blog/Blog.jsx
+++ gpt3/src/containers/blog/Blog.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './blog.css';
+import { Article } from '../../components';
 import blog1 from '../../assets/blog01.png'
 import blog2 from '../../assets/blog02.png'
 import blog3 from '../../assets/blog03.png'
@@ -9,61 +10,36 @@ import blog5 from '../../assets/blog05.png'
 const Blog = () => {
   return (
     <div className='blog-container' id='blog'>
-      <h2 className='gradient-text'>A lot is happening, <br /> We are blogging about it.</h2>
-      <div className='blog-container-stories'>
-        <div className="blog-story-left">
-          <div className="blog-card-tall">
-            <img src={blog1} alt="tunnel" />
-            <div className='title-section'>
-              <div>
-                <p className="blog-date">Sep 26, 2021</p>
-                <h2 className="blog-title">GPT-3 and Open  AI is the future. Let us exlore how it is?</h2>
-              </div>
-              <p className="blog-read">Read Full Article</p>
-            </div>
-          </div>
+      <h1 className='gradient__text blog-heading'>A lot is happening, <br />We are blogging about it.</h1>
+      <div className='gpt3__blog-container'>
+        <div className="gpt3__blog-container_groupA">
+          <Article 
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+            date="Sep 26, 2021"
+            image={blog1}
+          />
         </div>
-        <div className='blog-story-right'>
-          <div className="blog-card">
-              <img src={blog2} alt="face" />
-              <div className='title-section'>
-              <div>
-                <p className="blog-date">Sep 26, 2021</p>
-                <h2 className="blog-title">GPT-3 and Open  AI is the future. Let us exlore how it is?</h2>
-              </div>
-              <p className="blog-read">Read Full Article</p>
-            </div>
-          </div>
-          <div className="blog-card">
-            <img src={blog3} alt="link" />
-            <div className='title-section'>
-              <div>
-                <p className="blog-date">Sep 26, 2021</p>
-                <h2 className="blog-title">GPT-3 and Open  AI is the future. Let us exlore how it is?</h2>
-              </div>
-              <p className="blog-read">Read Full Article</p>
-            </div>
-          </div>
-          <div className="blog-card">
-            <img src={blog4} alt="numbers" />
-            <div className='title-section'>
-              <div>
-                <p className="blog-date">Sep 26, 2021</p>
-                <h2 className="blog-title">GPT-3 and Open  AI is the future. Let us exlore how it is?</h2>
-              </div>
-              <p className="blog-read">Read Full Article</p>
-            </div>
-          </div>
-          <div className="blog-card">
-            <img src={blog5} alt="warning" />
-            <div className='title-section'>
-              <div>
-                <p className="blog-date">Sep 26, 2021</p>
-                <h2 className="blog-title">GPT-3 and Open  AI is the future. Let us exlore how it is?</h2>
-              </div>
-              <p className="blog-read">Read Full Article</p>
-            </div>
-          </div>
+        <div className="gpt3__blog-container_groupB">
+          <Article 
+              title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+              date="Sep 26, 2021"
+              image={blog2}
+          />
+          <Article 
+              title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+              date="Sep 26, 2021"
+              image={blog3}
+          />
+          <Article 
+              title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+              date="Sep 26, 2021"
+              image={blog4}
+          />
+          <Article 
+              title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+              date="Sep 26, 2021"
+              image={blog5}
+          />
         </div>
       </div>
     </div>
